035f959ea143b62c1d54fd1ff5f5884e8084bc38
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-02-09 17:12:12 -0800

    assignment2 padding fix

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const app = express();
 
 let src = '/src/assets/views/';
 
-let login = 'user/login.html';
-let profile = 'user/profile.html';
-let resgister = 'user/register.html';
-let pgedit = 'page/page-edit.html';
-let pglist = 'page/page-list.html';
-let pgnew = 'page/page-new.html';
-let wbedit = 'website/website-edit.html';
-let wblist = 'website/website-list.html';
-let wbnew = 'website/website-new.html';
-let wdch = 'widget/widget-chooser.html';
-let wdhd = 'widget/widget-heading.html';
-let wdim = 'widget/widget-image.html';
-let wdlist = 'widget/widget-list.html';
-let wdyt = 'widget/widget-youtube.html';
+let login = '/user/login.html';
+let profile = '/user/profile.html';
+let resgister = '/user/register.html';
+let pgedit = '/page/page-edit.html';
+let pglist = '/page/page-list.html';
+let pgnew = '/page/page-new.html';
+let wbedit = '/website/website-edit.html';
+let wblist = '/website/website-list.html';
+let wbnew = '/website/website-new.html';
+let wdch = '/widget/widget-chooser.html';
+let wdhd = '/widget/widget-heading.html';
+let wdim = '/widget/widget-image.html';
+let wdlist = '/widget/widget-list.html';
+let wdyt = '/widget/widget-youtube.html';
 
 
 function modifiedSendFile (req, res, path) {
@@ -68,23 +68,26 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'src/assets/index.html'));
 });
 
+
 //app.get('/login.html', function (req, res) {
   //res.sendFile(path.join(__dirname, 'src/assets/views/user/login.html'));
 //});
 
+app.use(express.static(__dirname + '/src/assets/views/css'));
+
 app.get(login, (req, res)=>{ modifiedSendFile(req, res,login);});
 app.get(profile, (req, res)=>{ modifiedSendFile(req, res,profile);});
-app.get(resgister, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(pgedit, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(pglist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(pgnew, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wbedit, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wblist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wbnew, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wdch, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wdhd, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wdim, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wdlist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
-app.get(wdyt, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(resgister, (req, res)=>{ modifiedSendFile(req, res, resgister);});
+app.get(pgedit, (req, res)=>{ modifiedSendFile(req, res, pgedit);});
+app.get(pglist, (req, res)=>{ modifiedSendFile(req, res, pglist);});
+app.get(pgnew, (req, res)=>{ modifiedSendFile(req, res, pgnew);});
+app.get(wbedit, (req, res)=>{ modifiedSendFile(req, res, wbedit);});
+app.get(wblist, (req, res)=>{ modifiedSendFile(req, res, wblist);});
+app.get(wbnew, (req, res)=>{ modifiedSendFile(req, res, wbnew);});
+app.get(wdch, (req, res)=>{ modifiedSendFile(req, res, wdch);});
+app.get(wdhd, (req, res)=>{ modifiedSendFile(req, res, wdhd);});
+app.get(wdim, (req, res)=>{ modifiedSendFile(req, res, wdim);});
+app.get(wdlist, (req, res)=>{ modifiedSendFile(req, res, wdlist);});
+app.get(wdyt, (req, res)=>{ modifiedSendFile(req, res, wdyt);});
 
 server.listen( port , () => console.log('Running on port 5000'));
