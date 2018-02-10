10b1e52833ca3cb5b78e9b054dc522601e4ce788
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-02-09 16:00:55 -0800

    assignment2 filehandler

--- server.js
+++ server.js
@@ -7,6 +7,30 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
 
+let src = '/src/assets/views/';
+
+let login = 'user/login.html';
+let profile = 'user/profile.html';
+let resgister = 'user/register.html';
+let pgedit = 'page/page-edit.html';
+let pglist = 'page/page-list.html';
+let pgnew = 'page/page-new.html';
+let wbedit = 'website/website-edit.html';
+let wblist = 'website/website-list.html';
+let wbnew = 'website/website-new.html';
+let wdch = 'widget/widget-chooser.html';
+let wdhd = 'widget/widget-heading.html';
+let wdim = 'widget/widget-image.html';
+let wdlist = 'widget/widget-list.html';
+let wdyt = 'widget/widget-youtube.html';
+
+
+function modifiedSendFile (req, res, path) {
+  path = src+path;
+  res.sendFile(__dirname + path);
+}
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,17 +62,29 @@ const server = http.createServer(app);
 
 
 
-
 // For Build: Catch all other routes and return the index file -- BUILDING
 app.get('/', function (req, res) {
   //res.sendFile(path.join(__dirname, 'dist/index.html'));
   res.sendFile(path.join(__dirname, 'src/assets/index.html'));
 });
 
-app.get('/login.html', function (req, res) {
-  //res.sendFile(path.join(__dirname, 'dist/index.html'));
-  res.sendFile(path.join(__dirname, 'src/assets/views/user/login.html'));
-});
+//app.get('/login.html', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'src/assets/views/user/login.html'));
+//});
 
+app.get(login, (req, res)=>{ modifiedSendFile(req, res,login);});
+app.get(profile, (req, res)=>{ modifiedSendFile(req, res,profile);});
+app.get(resgister, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(pgedit, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(pglist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(pgnew, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wbedit, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wblist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wbnew, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wdch, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wdhd, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wdim, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wdlist, (req, res)=>{ modifiedSendFile(req, res,resgister);});
+app.get(wdyt, (req, res)=>{ modifiedSendFile(req, res,resgister);});
 
 server.listen( port , () => console.log('Running on port 5000'));
