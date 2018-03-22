c5832e249b6946db018dbb14e8efb9680ca1f889
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-03-21 21:35:57 -0700

    check

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -158,9 +158,9 @@ module.exports = function (app) {
     widget.url = filename;
     //console.log(widget.url);
     //console.log("o:"+originalname + " f:" + filename + " p:" + path + " d:" + destination);
-    var callbackUrl = 'profile/' + userId + '/websitelist/' + websiteId + '/pagelist/' + pageId + '/widgetlist';
+    var callbackUrl = 'https://cs5610-webdev-app.herokuapp.com/profile/' + userId + '/websitelist/' + websiteId + '/pagelist/' + pageId + '/widgetlist';
 
     console.log(callbackUrl);
-    //res.redirect(callbackUrl);
+    res.redirect(callbackUrl);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: "https://cs5610-webdev-app.herokuapp.com"
 };
