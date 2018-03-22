e1771949a2217b827442eb563d2361ec6946b3b5
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-03-21 23:01:28 -0700

    final1

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -158,7 +158,7 @@ module.exports = function (app) {
     widget.url = filename;
     //console.log(widget.url);
     //console.log("o:"+originalname + " f:" + filename + " p:" + path + " d:" + destination);
-    var callbackUrl = "http://localhost:4200/profile/" + userId + "/websitelist/"
+    var callbackUrl = "http://cs5610-webdev-app.herokuapp.com/profile/" + userId + "/websitelist/"
       + websiteId + "/pagelist/" + pageId + "/widgetlist";
 
     console.log(callbackUrl);
