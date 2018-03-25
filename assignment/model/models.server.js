edae3ececeaea698f0b6d915f08ee67bea15df74
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-03-25 14:48:55 -0700

    heroku db connection

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var uri;
 
-if(process.env.NODE_ENV == "prod")
-  uri = process.env.MONGODB_URI;
-else
-  uri = 'mongodb://localhost:27017/webdev';
+// if(process.env.NODE_ENV == "prod")
+//   uri = process.env.MONGODB_URI;
+// else
+//   uri = 'mongodb://localhost:27017/webdev';
 
+uri = 'mongodb://ds123259.mlab.com:23259/heroku_87znt65z/webdev';
+// uri = 'mongodb://localhost:27017/webdev';
 var db = mongoose.connect(uri);
 module.exports = db;
