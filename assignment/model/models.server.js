9b4a9c73b6a23ab21bc343abd30e8822b033dc88
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-03-25 15:41:43 -0700

    db updated uri

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -6,7 +6,8 @@ var uri;
 // else
 //   uri = 'mongodb://localhost:27017/webdev';
 
-uri = 'mongodb://ds123259.mlab.com:23259/heroku_87znt65z/webdev';
+// uri = 'mongodb://ds123259.mlab.com:23259/heroku_87znt65z/webdev';
+uri = 'mongodb://heroku_87znt65z:u1hkh6brfj9bubuk2s3uitk0f7@ds123259.mlab.com:23259/heroku_87znt65z'
 // uri = 'mongodb://localhost:27017/webdev';
 var db = mongoose.connect(uri);
 module.exports = db;
