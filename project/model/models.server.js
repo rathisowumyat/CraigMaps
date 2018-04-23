7133589175f92bfc95dfb7f733320899d376909c
rathisowumyat <rathisowumyat@ccs.neu.edu>
2018-04-23 08:16:39 -0700

    Integrated with 2 external APIs (Craigslist + Google Maps)
    Integrated with 2 external APIs (Craigslist + Google Maps)

--- project/model/models.server.js
+++ project/model/models.server.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+//var uri = 'mongodb://localhost:27017/craigmaps';
+ var uri = 'mongodb://heroku_k8jhrs71:h1423c2t0jntre1901gu1fm45l@ds119489.mlab.com:19489/heroku_k8jhrs71'; //process.env.MONGODB_URI;
 
 var db = mongoose.connect(uri);
 module.exports = db;
